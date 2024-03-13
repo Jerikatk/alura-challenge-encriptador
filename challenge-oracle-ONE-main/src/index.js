54dffab02d0fae6c1b8add9e1c03e29f72808f45
Jeison Vera <jw.vera.r@gmail.com>
2024-03-12 18:58:52 -0700

    format

--- challenge-oracle-ONE-main/src/index.js
+++ challenge-oracle-ONE-main/src/index.js
@@ -1,36 +1,29 @@
-
-import  darkTheme  from "./components/dark-theme.js";
+import darkTheme from "./components/dark-theme.js";
 import handleEncrypt from "./components/encrypt.js";
 import handleDecrypt from "./components/decrypt.js";
 import { handleKeyup } from "./components/handleError.js";
-import  handleCopy  from "./components/buttonCopy.js";
+import handleCopy from "./components/buttonCopy.js";
 import handleModal from "./components/buttonAsk.js";
 
-
-
-
-
-document.addEventListener("DOMContentLoaded",()=>{
- 
-  //function encrypter 
+document.addEventListener("DOMContentLoaded", () => {
+  //function encrypter
 
   handleEncrypt();
 
-//function decrypter 
+  //function decrypter
   handleDecrypt();
 
   //validate error
 
   handleKeyup();
- ///
+  ///
   //function button copy
   handleCopy();
 
-   
-   //ask modal
+  //ask modal
 
-   handleModal();
-})
+  handleModal();
+});
 
 //ThemeDark();
-darkTheme('.header-nav-theme','dark-mode');
+darkTheme(".header-nav-theme", "dark-mode");
